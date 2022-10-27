b31f5cd29cbf06eefe6d28643da9f491caf0c72f
zhangyuchi <zhangyc110@chinatelecom.cn>
2022-10-27 12:10:38 +0800

    test

---
+++ untitled/src/main/java/gitTest.java
@@ -0,0 +1,14 @@
+/**
+ * @projectName: untitled
+ * @package: PACKAGE_NAME
+ * @className: gitTest
+ * @author: zyc
+ * @description: TODO
+ * @date: 2022/10/27 7:58
+ * @version: 1.0
+ */
+public class gitTest {
+    public static void main(String[] args) {
+
+    }
+}
